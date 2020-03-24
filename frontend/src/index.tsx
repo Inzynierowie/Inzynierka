1a8b26a269cb7fe8a56e572f0406368026ba4f0c
lukortech <lukortech@wp.pl>
2020-03-24 08:37:31 +0100

    Initial frontend commit. Tests are broken, but will address that later

---
+++ frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Home } from "../pages";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<Home />, document.getElementById("root"));
+
+serviceWorker.unregister();
