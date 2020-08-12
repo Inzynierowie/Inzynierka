ce74ac9edd8ad88f1e83b9b9c42af9f2d524f1b0
merkenter <37770452+merkenter@users.noreply.github.com>
2020-08-12 21:24:15 +0200

    Task add tables and basic sql querry (#18)
    * Add first db restor point
    
    * add basic sql query

--- backend/src/main/java/com/engineering/thesis/backend/BackendApplication.java
+++ backend/src/main/java/com/engineering/thesis/backend/BackendApplication.java
@@ -2,7 +2,15 @@ package com.engineering.thesis.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.engineering.thesis.backend.repositories.PatientsRepository;
+
+@EnableAutoConfiguration
+@ComponentScan
+@EnableJpaRepositories(basePackageClasses = PatientsRepository.class)
 @SpringBootApplication
 public class BackendApplication {
 
