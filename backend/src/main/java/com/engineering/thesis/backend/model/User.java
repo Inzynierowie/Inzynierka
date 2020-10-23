cd15898b81a2d26907ed08336738049d456a83f5
justam92 <31111330+justam92@users.noreply.github.com>
2020-10-23 20:20:50 +0200

    created User entity

---
+++ backend/src/main/java/com/engineering/thesis/backend/model/User.java
@@ -0,0 +1,18 @@
+package com.engineering.thesis.backend.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+}
