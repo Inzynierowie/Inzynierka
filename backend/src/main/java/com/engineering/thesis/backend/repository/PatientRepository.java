ac0e88bb6c267e78e3c1de6de081799c9c4259c3
justam92 <31111330+justam92@users.noreply.github.com>
2020-10-25 16:47:19 +0100

    refactor code

--- backend/src/main/java/com/engineering/thesis/backend/repository/PatientRepository.java
+++ backend/src/main/java/com/engineering/thesis/backend/repository/PatientRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient,Long> {
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
