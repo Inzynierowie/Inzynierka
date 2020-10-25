3d44835a67d4a8086c3911077622276e93cb1d4f
justam92 <31111330+justam92@users.noreply.github.com>
2020-10-25 16:48:32 +0100

    refactor code

--- backend/src/main/java/com/engineering/thesis/backend/service/AppointmentService.java
+++ backend/src/main/java/com/engineering/thesis/backend/service/AppointmentService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface AppointmentService {
     void create(Appointment appointment);
+
     void deleteById(Long id);
+
     List<Appointment> selectAll();
+
     Appointment selectAppointmentById(Long id);
 }
