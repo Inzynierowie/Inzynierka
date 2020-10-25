0977a6ef96aac1a9faf7ae0a61bdc2ce71dc318a
justam92 <31111330+justam92@users.noreply.github.com>
2020-10-25 17:43:15 +0100

    remove api from endpoint

--- backend/src/main/java/com/engineering/thesis/backend/controller/MedicalFacilityController.java
+++ backend/src/main/java/com/engineering/thesis/backend/controller/MedicalFacilityController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/medicalFacility")
+@RequestMapping("medicalFacility")
 public class MedicalFacilityController {
 
     @Autowired
