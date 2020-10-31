c7cd8e77d2dc42c44ba551bf4336e590c2ce8344
justam92 <31111330+justam92@users.noreply.github.com>
2020-10-31 20:08:30 +0100

    create UserRepository

---
+++ backend/src/main/java/com/engineering/thesis/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.engineering.thesis.backend.repository;
+
+import com.engineering.thesis.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
