d3ca78cc744290db4e557f5b370cb514556dfcce
justam92 <31111330+justam92@users.noreply.github.com>
2020-10-31 20:08:56 +0100

    create UserDetailsServiceImpl

---
+++ backend/src/main/java/com/engineering/thesis/backend/serviceImpl/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.engineering.thesis.backend.serviceImpl;
+
+import com.engineering.thesis.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email) ;
+    }
+}
