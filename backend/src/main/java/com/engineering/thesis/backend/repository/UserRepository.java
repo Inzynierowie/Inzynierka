f5fcffc3bedb35658ca751a16edb69c4503db4ac
justam92 <31111330+justam92@users.noreply.github.com>
2020-11-07 20:51:19 +0100

    added methods

--- backend/src/main/java/com/engineering/thesis/backend/repository/UserRepository.java
+++ backend/src/main/java/com/engineering/thesis/backend/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.engineering.thesis.backend.repository;
 import com.engineering.thesis.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
