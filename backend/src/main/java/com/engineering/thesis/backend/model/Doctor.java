3b4baf0d6aa65487becef45cebaa21e91c8d32df
justam92 <31111330+justam92@users.noreply.github.com>
2020-11-08 23:05:03 +0100

    Changed extend User on join column from sql

--- backend/src/main/java/com/engineering/thesis/backend/model/Doctor.java
+++ backend/src/main/java/com/engineering/thesis/backend/model/Doctor.java
@@ -1,19 +1,22 @@
 package com.engineering.thesis.backend.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 @Entity(name = "doctors")
-public class Doctor extends User {
+public class Doctor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "Fk_user") )
+    private User user;
+
     private String specialist;
 
 }
