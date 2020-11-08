5d195def3fa212d5d7cf2c5053971617e1427d4d
justam92 <31111330+justam92@users.noreply.github.com>
2020-11-08 19:10:29 +0100

    refactor code

--- backend/src/main/java/com/engineering/thesis/backend/model/Doctor.java
+++ backend/src/main/java/com/engineering/thesis/backend/model/Doctor.java
@@ -1,19 +1,19 @@
 package com.engineering.thesis.backend.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Entity(name = "doctors")
-public class Doctor {
+public class Doctor extends User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
-    private String name;
-    private String surname;
     private String specialist;
 
 }
