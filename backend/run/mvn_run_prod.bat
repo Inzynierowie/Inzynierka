148bcb1a59cba8643c4c72cf0e470d3b6f83547f
merkenter <m.zabielski11@o2.pl>
2020-11-13 22:05:25 +0100

    change default port to 8010 add additional desc to readme and add more fill text to test_data

--- backend/run/mvn_run_prod.bat
+++ backend/run/mvn_run_prod.bat
@@ -1,2 +1,2 @@
 cd ..
-mvn spring-boot:run -Dspring-boot.run.arguments="--server.port=8080 --spring.jpa.hibernate.ddl-auto=none"
+mvn spring-boot:run -Dspring-boot.run.arguments="--server.port=8010 --spring.jpa.hibernate.ddl-auto=none"
