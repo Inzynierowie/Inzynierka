c664dbf8413ca82daad516bbabcc59b77e0e6bbc
justam92 <31111330+justam92@users.noreply.github.com>
2020-11-13 18:06:51 +0100

    created UserDetailsServiceImpl class with method loadUserByUsername

---
+++ backend/src/main/java/com/engineering/thesis/backend/serviceImpl/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.engineering.thesis.backend.serviceImpl;
+
+import com.engineering.thesis.backend.model.User;
+import com.engineering.thesis.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+
+        user.orElseThrow(() -> new UsernameNotFoundException(email + " not found."));
+        return user.get();
+    }
+}
