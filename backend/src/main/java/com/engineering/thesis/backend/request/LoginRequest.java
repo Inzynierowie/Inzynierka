de9576cb75529332448c6e11147aebc0926aff5f
justam92 <31111330+justam92@users.noreply.github.com>
2020-11-13 16:35:44 +0100

    created LoginRequest class

---
+++ backend/src/main/java/com/engineering/thesis/backend/request/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.engineering.thesis.backend.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class LoginRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
