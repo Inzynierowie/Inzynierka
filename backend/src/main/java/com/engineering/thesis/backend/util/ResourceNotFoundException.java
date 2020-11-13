e9e95e483532db86b1a98cf4011e45e9564abe5c
justam92 <31111330+justam92@users.noreply.github.com>
2020-11-13 16:34:58 +0100

    created ResourceNotFoundException class

---
+++ backend/src/main/java/com/engineering/thesis/backend/util/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.engineering.thesis.backend.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception {
+    private static final long serialVersionUID = 1l;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
