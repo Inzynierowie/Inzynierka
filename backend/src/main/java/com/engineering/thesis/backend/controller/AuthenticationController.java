75d1f70964b32994d4aaa0d6e141141bd6a03ba0
justam92 <31111330+justam92@users.noreply.github.com>
2020-12-17 23:18:37 +0100

    created refreshToken method

--- backend/src/main/java/com/engineering/thesis/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/engineering/thesis/backend/controller/AuthenticationController.java
@@ -1,20 +1,24 @@
 package com.engineering.thesis.backend.controller;
 
+import com.engineering.thesis.backend.config.jwt.JwtToken;
 import com.engineering.thesis.backend.model.authentication.LoginRequest;
 import com.engineering.thesis.backend.model.authentication.RegisterRequest;
 import com.engineering.thesis.backend.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final JwtToken jwtToken;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest data) {
@@ -24,5 +28,11 @@ public class AuthenticationController {
     @PostMapping("/register")
     public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest signUpRequest) {
         return authenticationService.register(signUpRequest);
+    }
+
+    @GetMapping("/refreshToken")
+    public ResponseEntity<String> refreshToken(HttpServletRequest request) {
+        String userEmail = (String) request.getAttribute("userEmail");
+        return ResponseEntity.ok(jwtToken.generateRefreshToken(userEmail));
     }
 }
